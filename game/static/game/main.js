581b36c5eecec191ad1db67718767bd0debf9545
jtwaugh <james.t.waugh@gmail.com>
2023-05-07 18:06:10 -0400

    Fix settlement display issue. Add game speed. Precompute settlement updates for performance. Confirm that settlements pop into existence. Update terrain map. Add folder with preliminary historical maps.

--- game/static/game/main.js
+++ game/static/game/main.js
@@ -1,376 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let settlements = [
-        {
-          "id": 2,
-          "properties": {
-            "name": "Çatalhöyük",
-            "founding": 7400,
-            "type": "settlement"
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [32.827679, 37.668047]
-          },
-          "population": {
-            "7400": 3000,
-            "7300": 3500,
-            "7200": 4000,
-            "7100": 4500,
-            "7000": 5000,
-            "6900": 5500
-          }
-        },
-        {
-          "id": 3,
-          "properties": {
-            "name": "Hunter-Gatherer Settlements",
-            "founding": 11000,
-            "type": "settlement"
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [36.456382, 40.222036]
-          },
-          "population": {
-            "11000": 80,
-            "10900": 85,
-            "10800": 90,
-            "10700": 95
-          }
-        },
-        {
-          "id": 4,
-          "properties": {
-            "name": "Jericho",
-            "founding": 9600,
-            "type": "settlement"
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [35.444362, 31.846850]
-          },
-          "population": {
-            "9600": 2000,
-            "9500": 2200,
-            "9400": 2300,
-            "9300": 2400,
-            "9200": 2500,
-            "9100": 2600
-          }
-        },
-        {
-          "id": 5,
-          "properties": {
-            "name": "Hunter-Gatherer Settlements",
-            "founding": 10600,
-            "type": "settlement"
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [28.709477, 44.872065]
-          },
-          "population": {
-            "10600": 70,
-            "10500": 75,
-            "10400": 80,
-            "10300": 85
-          }
-        },
-        {
-          "id": 6,
-          "properties": {
-            "name": "Göbekli Tepe",
-            "founding": 9600,
-            "type": "settlement"
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [38.922547, 37.223300]
-          },
-          "population": {
-            "9600": 300,
-            "9500": 350,
-            "9400": 400,
-            "9300": 450,
-            "9200": 500,
-            "9100": 550
-          }
-        },
-        {
-          "id": 7,
-          "properties": {
-            "name": "Hunter-Gatherer Settlements",
-            "founding": 10200,
-            "type": "settlement"
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [1.276750, 48.357550]
-          },
-          "population": {
-            "10200": 60
-          }
-        },
-        {
-            "id": 8,
-            "properties": {
-              "name": "Hunter-Gatherer Settlements",
-              "founding": 10200,
-              "type": "settlement"
-            },
-            "geometry": {
-              "type": "Point",
-              "coordinates": [1.276750, 48.357550]
-            },
-            "population": {
-              "10200": 60,
-              "10100": 65,
-              "10000": 70,
-              "9900": 75
-            }
-        },
-        {
-            "id": 9,
-            "properties": {
-                "name": "Ain Ghazal",
-                "founding": 7250,
-                "type": "settlement"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [35.893529, 31.992472]
-            },
-            "population": {
-                "7250": 1300,
-                "7200": 1400,
-                "7150": 1500,
-                "7100": 1600,
-                "7050": 1700,
-                "7000": 1800
-            }
-        },
-        {
-            "id": 10,
-            "properties": {
-                "name": "Hunter-Gatherer Settlements",
-                "founding": 9800,
-                "type": "settlement"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [10.594290, 57.708870]
-            },
-            "population": {
-                "9800": 50,
-                "9700": 55,
-                "9600": 60,
-                "9500": 65
-            }
-        },
-        {
-            "id": 11,
-            "properties": {
-                "name": "Tell Sabi Abyad",
-                "founding": 6100,
-                "type": "settlement"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [38.702020, 36.536990]
-            },
-            "population": {
-                "6100": 1200,
-                "6050": 1250,
-                "6000": 1300,
-                "5950": 1350,
-                "5900": 1400,
-                "5850": 1450
-            }
-        },
-        {
-            "id": 12,
-            "properties": {
-              "name": "Hunter-Gatherer Settlements",
-              "founding": 9400,
-              "type": "settlement"
-            },
-            "geometry": {
-              "type": "Point",
-              "coordinates": [-3.703790, 40.416775]
-            },
-            "population": {
-              "9400": 40,
-              "9300": 45,
-              "9200": 50,
-              "9100": 55
-            }
-        },
-        {
-            "id": 13,
-            "properties": {
-                "name": "Beidha",
-                "founding": 7000,
-                "type": "settlement"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [35.445346, 30.334087]
-            },
-            "population": {
-                "7000": 1100,
-                "6950": 1150,
-                "6900": 1200,
-                "6850": 1250
-            }
-         }
-    ]
-
-    let tech_tree = technology_tree = {
-      "basic_tools": {
-          "dependencies": [],
-          "unlocks": ["agriculture", "basic_pottery", "basic_hunting"],
-      },
-      "basic_pottery": {
-          "dependencies": ["basic_tools"],
-          "unlocks": [
-              "Cardium pottery",
-              "Linear pottery",
-              "Vinča pottery",
-              "Funnel beaker pottery",
-              "Cucuteni-Trypillia pottery",
-              "Yamnaya pottery",
-          ],
-      },
-      "agriculture": {
-          "dependencies": ["basic_tools"],
-          "unlocks": ["animal_husbandry", "irrigation", "plant_domestication"],
-      },
-      "animal_husbandry": {
-          "dependencies": ["agriculture"],
-          "unlocks": ["pastoralism", "horse_domestication"],
-      },
-      "basic_hunting": {
-          "dependencies": ["basic_tools"],
-          "unlocks": ["marine_resources"],
-      },
-      "plant_domestication": {
-          "dependencies": ["agriculture"],
-          "unlocks": [],
-      },
-      "irrigation": {
-          "dependencies": ["agriculture"],
-          "unlocks": ["advanced_agriculture"],
-      },
-      "pastoralism": {
-          "dependencies": ["animal_husbandry"],
-          "unlocks": [],
-      },
-      "horse_domestication": {
-          "dependencies": ["animal_husbandry"],
-          "unlocks": ["wheeled_vehicles"],
-      },
-      "advanced_agriculture": {
-          "dependencies": ["irrigation"],
-          "unlocks": ["large_settlements"],
-      },
-      "marine_resources": {
-          "dependencies": ["basic_hunting"],
-          "unlocks": [],
-      },
-      "large_settlements": {
-          "dependencies": ["advanced_agriculture"],
-          "unlocks": ["megalithic_tombs", "amber_trade"],
-      },
-      "wheeled_vehicles": {
-          "dependencies": ["horse_domestication"],
-          "unlocks": [],
-      },
-      "megalithic_tombs": {
-          "dependencies": ["large_settlements"],
-          "unlocks": [],
-      },
-      "amber_trade": {
-          "dependencies": ["large_settlements"],
-          "unlocks": [],
-      },
-      "metallurgy": {
-          "dependencies": ["basic_tools"],
-          "unlocks": ["copper_working"],
-      },
-      "copper_working": {
-          "dependencies": ["metallurgy"],
-          "unlocks": [],
-      },
-    }
-
-    let gene_pools = ["WHG", "EEF", "EHG", "CHG", "ANF", "SP"]
-
-    let neolithic_cultures = [
-      {
-          "name": "Cardium Pottery Culture",
-          "gene_pool": {"EEF": 0.9, "WHG": 0.1},
-          "material_practices": [
-              "Cardium pottery",
-              "agriculture",
-              "animal husbandry",
-              "marine resources",
-          ],
-      },
-      {
-          "name": "Linear Pottery Culture (LBK)",
-          "gene_pool": {"EEF": 0.95, "WHG": 0.05},
-          "material_practices": [
-              "Linear pottery",
-              "agriculture",
-              "animal husbandry",
-              "longhouses",
-          ],
-      },
-      {
-          "name": "Vinča Culture",
-          "gene_pool": {"EEF": 0.9, "ANF": 0.1},
-          "material_practices": [
-              "Vinča pottery",
-              "agriculture",
-              "animal husbandry",
-              "metallurgy (copper)",
-              "large settlements",
-          ],
-      },
-      {
-          "name": "Funnel Beaker Culture (TRB)",
-          "gene_pool": {"EEF": 0.6, "WHG": 0.3, "EHG": 0.1},
-          "material_practices": [
-              "Funnel beaker pottery",
-              "agriculture",
-              "animal husbandry",
-              "megalithic tombs",
-              "amber trade",
-          ],
-      },
-      {
-          "name": "Cucuteni-Trypillia Culture",
-          "gene_pool": {"EEF": 0.7, "WHG": 0.2, "CHG": 0.1},
-          "material_practices": [
-              "Cucuteni-Trypillia pottery",
-              "agriculture",
-              "animal husbandry",
-              "large settlements",
-              "painted ceramics",
-          ],
-      },
-      {
-          "name": "Yamnaya Culture",
-          "gene_pool": {"EHG": 0.4, "CHG": 0.4, "SP": 0.2},
-          "material_practices": [
-              "Yamnaya pottery",
-              "pastoralism",
-              "horse domestication",
-              "kurgan burial mounds",
-              "wheeled vehicles",
-          ],
-      },
-    ]  
+    let settlements = []
 
     const popColors = d3.scaleOrdinal()
       .domain(
@@ -401,8 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
       )
 
     const terrainColors = d3.scaleOrdinal()
-      .domain([0, 1, 2, 3]) // Add as many terrain types as you have
-      .range(["#184a85", "#ff7f0e", "#2ca02c", "#d62728"]); // Add corresponding colors for each terrain type
+      .domain(["Ice", "Water", "Plains", "Mountains"]) // Add as many terrain types as you have
+      .range([  "#2ca02c", "#184a85", "#ff7f0e", "#d62728"]); // Add corresponding colors for each terrain type
 
     const koeppenColors = d3.scaleOrdinal()
       .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]) // Add as many terrain types as you have
@@ -448,8 +77,34 @@ document.addEventListener('DOMContentLoaded', () => {
       "population_density": popColors,
     };
 
-    let hexLayer = null;
-    let hexagonData = null;
+    let hexLayer;
+    let hexagonData;
+
+    let currentYear = -8000;
+    const speedSlider = document.getElementById("speed-slider");
+    const yearsPerSecondDisplay = document.getElementById("years-per-second");
+    let intervalID;
+
+    speedSlider.addEventListener("input", () => {
+      adjustGameSpeed();
+    });
+
+    function adjustGameSpeed() {
+      clearInterval(intervalID);
+      
+      const yearsPerSecond = parseFloat(speedSlider.value);
+
+      const intervalDuration = 1 / yearsPerSecond; // Seconds per year
+      
+      yearsPerSecondDisplay.innerText = yearsPerSecond;
+
+      intervalID = setInterval(() => {
+        currentYear++;
+        currentYearDisplay.innerText = currentYear;
+        updateVisibleSettlements();
+      }, intervalDuration * 1000); // Convert seconds to milliseconds
+    }
+
 
     function populateMapModeSelector() {
       const MapModeSelectorControl = L.Control.extend({
@@ -487,9 +142,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
 
-    let currentYear = -7500;
-    let intervalID;
-
     // Create a Leaflet map
     const map = L.map('map').setView([39.0742, 21.8243], 4); // Set the center and zoom level
     
@@ -499,7 +151,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function drawHexagons(data, selectedMode) {
       const selectedColorScheme = colorSchemes[selectedMode];
 
-      console.log(data)
       hexLayer = L.geoJSON(data, {
         style: function (feature) {
           return {
@@ -555,15 +206,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const startYearInput = document.getElementById('start-year');
     const startGameButton = document.getElementById('start-game');
     const pausePlayButton = document.getElementById('pause-play');
-    const closeInfoButton = document.getElementById('close-info');
     const mapContainer = document.getElementById('map-container');
     const currentYearDisplay = document.getElementById('current-year');
-    const settlementInfo = document.getElementById('settlement-info');
+    const entityInfo = document.getElementById('entity-info');
+    const settlementInfo = document.querySelector('.settlement-info');
+    const dnaInfo = document.querySelector('.dna-info');
     const settlementName = document.getElementById('settlement-name');
     const settlementPopulation = document.getElementById('settlement-population');
     const settlementLayer = L.layerGroup();
 
     settlementLayer.addTo(map);
+
+    let settlementUpdates = {};
 
     fetch('/static/game/assets/hex_pop.geojson')
     .then(response => response.json())
@@ -575,6 +229,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .catch(error => console.error('Error fetching GeoJSON data:', error));
 
     function createSettlement(settlement) {
+      if (settlement.properties.founding <= currentYear) {
         // Create an icon for the settlement
         const settlementIcon = L.icon({
           iconUrl: '/static/game/assets/settlement-icon.png',
@@ -591,31 +246,93 @@ document.addEventListener('DOMContentLoaded', () => {
         settlementLayer.addLayer(marker);
       
         // Attach a click event to the marker
-        marker.on('click', () => {
-            settlementName.textContent = settlement.properties.name;
-            const currentPopulation = settlement.population_history.find(
-                entry => entry.year <= currentYear
-            );
-            settlementPopulation.innerText = `Population: ${currentPopulation.population}`;
-            settlementInfo.style.display = 'block';
+        marker.on("click", () => {
+          console.log("Marker clicked");
+          settlementName.textContent = settlement.properties.name;
+          const populationYears = Object.keys(settlement.properties.population).map(Number);
+          const validYears = populationYears.filter((year) => year <= currentYear);
+          
+          if (validYears.length > 0) {
+            const latestYear = Math.max(...validYears);
+            const currentPopulation = settlement.properties.population[latestYear];
+            settlementPopulation.innerText = `Population: ${currentPopulation}`;
+          } else {
+            settlementPopulation.innerText = `Population: N/A`;
+          }
+
+          settlementInfo.style.display = 'block';
+          dnaInfo.style.display = 'none';
         });
       }
+    }
 
+    function prepareSettlementUpdates() {
+      fetch('/static/game/assets/settlements.geojson')
+        .then(response => response.json())
+        .then(data => {
+          // Call the drawHexagons function with the fetched GeoJSON data
+          settlements = data;
+
+          settlements.forEach((settlement) => {
+            const foundingYear = settlement.properties.founding;
+        
+            if (!settlementUpdates[foundingYear]) {
+              settlementUpdates[foundingYear] = {
+                settlements: [],
+                populationUpdates: {},
+              };
+            }
+        
+            settlementUpdates[foundingYear].settlements.push(settlement);
+        
+            const populationYears = Object.keys(settlement.properties.population).map(Number);
+            populationYears.forEach((year) => {
+              if (!settlementUpdates[year]) {
+                settlementUpdates[year] = {
+                  settlements: [],
+                  populationUpdates: {},
+                };
+              }
+              settlementUpdates[year].populationUpdates[settlement.id] = settlement.properties.population[year];
+            });
+          });
+        })
+        .catch(error => console.error('Error fetching GeoJSON data:', error)); 
+    }
+
+    function updateVisibleSettlements() {
+      if (settlementUpdates[currentYear]) {
+        settlementUpdates[currentYear].settlements.forEach(settlement => {
+            createSettlement(settlement)
+        });
+      }
+    }
 
     function startGame() {
         startScreen.style.display = 'none';
         gameScreen.style.display = 'block';
+        
         currentYear = parseInt(startYearInput.value, 10);
         currentYearDisplay.innerText = currentYear;
         document.getElementById("hexInfo").style.display = "block";
 
-        settlements.forEach(settlement => {
-            createSettlement(settlement);
+        prepareSettlementUpdates();
+        
+        fetch('/static/game/assets/settlements.geojson')
+        .then(response => response.json())
+        .then(data => {
+          // Call the drawHexagons function with the fetched GeoJSON data
+          settlements = data;
+
+          settlements.forEach((settlement) => {
+            const foundingYear = settlement.properties.founding;
+            if (foundingYear <= currentYear) {
+              createSettlement(settlement);
+            }
+          });
         });
-        intervalID = setInterval(() => {
-            currentYear++;
-            currentYearDisplay.innerText = currentYear;
-        }, 1000);
+
+        adjustGameSpeed();
     }
 
     function toggleGamePause() {
@@ -694,7 +411,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     startGameButton.addEventListener('click', startGame);
     pausePlayButton.addEventListener('click', toggleGamePause);
-    closeInfoButton.addEventListener('click', hideSettlementInfo);
 });
 
 
